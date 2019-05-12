14dc5e43db115562c3344c9608f5afdf1780cd65
Rorian <dev@rorian.ru>
2019-05-12 15:48:24 +0300

    Added basic GUI. Added actions: undo, redo, close, cut, copy, paste, select all

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,87 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->plainTextEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    // create new file
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    // open file
+}
+
+void MainWindow::on_actionClose_triggered()
+{
+    // close file
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    // save file
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    // save file as...
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    // undo action
+    ui->plainTextEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    // redo action
+    ui->plainTextEdit->redo();
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    // exit program
+    this->close();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    // copy action
+    ui->plainTextEdit->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    // cut action
+    ui->plainTextEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    // paste action
+    ui->plainTextEdit->paste();
+}
+
+void MainWindow::on_actionDelete_triggered()
+{
+    // delete action
+}
+
+void MainWindow::on_actionSelect_all_triggered()
+{
+    // select all action
+    ui->plainTextEdit->selectAll();
+}
+
+void MainWindow::on_actionPrefereces_triggered()
+{
+    // preferences
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,35 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionClose_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionPrefereces_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionDelete_triggered();
+
+    void on_actionSelect_all_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
